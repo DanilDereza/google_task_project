295849be463390441eef0d4d375bc3a0ad6c6b8a
DanilDereza <Heydanil15@mail.ru>
2023-06-23 12:32:30 +1000

    create data access object

---
+++ app/src/main/java/com/example/google_task_project_dereza/data/database/DataAccessObject.kt
@@ -0,0 +1,35 @@
+package com.example.google_task_project_dereza.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.google_task_project_dereza.data.models.TaskDataModel
+import java.util.UUID
+
+@Dao
+interface DataAccessObject {
+
+    @Query("SELECT * FROM TaskDataModel WHERE isDone = 0 AND subtask IS NULL")
+    fun getTasks(): LiveData<List<TaskDataModel>>
+
+    @Query("SELECT * FROM TaskDataModel WHERE isDone = 0 AND subtask IS NULL AND isFavorite = 1")
+    fun getFavoriteTasks(): LiveData<List<TaskDataModel>>
+
+    @Query("SELECT * FROM TaskDataModel WHERE isDone = 1 AND subtask IS NULL")
+    fun getCompletedTasks(): LiveData<List<TaskDataModel>>
+
+    @Query("SELECT * FROM TaskDataModel WHERE subtask=(:parentID)")
+    fun getSubtasks(parentID: UUID): LiveData<List<TaskDataModel>>
+
+    @Insert
+    suspend fun addNewTask(task:TaskDataModel)
+
+    @Update
+    suspend fun updateDataTask(task: TaskDataModel)
+
+    @Delete
+    suspend fun deleteTask(task: TaskDataModel)
+}
