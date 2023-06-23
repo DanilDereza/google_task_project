de09e0794d99d9c3a827a399cc0f4f6462fef0ff
DanilDereza <Heydanil15@mail.ru>
2023-06-23 19:18:19 +1000

    create item_task.xml

---
+++ app/src/main/java/com/example/google_task_project_dereza/ui/MainPage/ListFragment/ListAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.google_task_project_dereza.ui.MainPage.ListFragment
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter():RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/example/google_task_project_dereza/ui/MainPage/MainPageFragment/MainPageFragment.kt
+++ app/src/main/java/com/example/google_task_project_dereza/ui/MainPage/MainPageFragment/MainPageFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.google_task_project_dereza.databinding.FragmentMainPageBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainPageFragment:Fragment() {
 
     private var _binding: FragmentMainPageBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var pagerAdapter: PagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +24,27 @@ class MainPageFragment:Fragment() {
         _binding = FragmentMainPageBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        pagerAdapter = PagerAdapter(this)
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.viewPager.currentItem = tab!!.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+        binding.viewPager.apply {
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.tabLayout.getTabAt(position)!!.select()
+                }
+            })
+        }
     }
 
     override fun onDetach() {
